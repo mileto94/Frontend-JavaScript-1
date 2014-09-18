c0faf57de2affbd162a50feef4e82256e640508e
milka <mileto94@abv.bg>
2014-09-18 17:17:25 +0300

    Add simple URL check

--- week1/gauntlet/3/app.js
+++ week1/gauntlet/3/app.js
@@ -2,8 +2,31 @@ $(document).ready(function() {
   "use strict";
   $("#search-button").click(function() {
     var url = $("#search-input").val();
-    var image = $("<img class='i' src=" + url + ">");
-    $(this).after(image);
+
+    if(url.indexOf("http://") > -1) {
+      var image = $("<img class='i' src=" + url + ">");
+      $(this).after(image);
+    }
+    else {
+      alert("Input Error - Invalid URL!");
+    }
+
+
+    // $("#1").onerror = function(){
+    //   alert("errror");
+    // };
+
+    // $.ajax({
+    //     crossOrigin: true,
+    //     url: url,
+    //     success: function(data) {
+    //         console.log(data);
+    //     },
+    //     error: function() {
+    //       alert("zsgf");
+    //     }
+    // });
+
 
     $("img").on("click", function() {
       $(this).remove();
