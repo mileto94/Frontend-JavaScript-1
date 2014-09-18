0632a3282027cd0396bac682110d07309fa8ef3f
milka <mileto94@abv.bg>
2014-09-18 15:22:11 +0300

    Upload images, show + delete them, no errors show

--- week1/gauntlet/3/app.js
+++ week1/gauntlet/3/app.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
+  "use strict";
+  $("#search-button").click(function() {
+    var url = $("#search-input").val();
+    var image = $("<img class='i' src=" + url + ">");
+    $(this).after(image);
 
+    $("img").on("click", function() {
+      $(this).remove();
+    });
+
+  });
 });
